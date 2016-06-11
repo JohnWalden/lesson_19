05384230aaf3b2734f96b673125ddbbb50d93fe2
John Walden <jwwkc7@mst.edu>
2016-06-11 16:41:02 +0000

    stuff

--- bloc/models/address_book.rb
+++ bloc/models/address_book.rb
@@ -7,7 +7,7 @@ class AddressBook
         @entries = []
     end
 
-     def add_entry(name, phone_number, email)
+    def add_entry(name, phone_number, email)
      index = 0
      entries.each do |entry|
        if name < entry.name
@@ -17,8 +17,8 @@ class AddressBook
      end
      # #11
      entries.insert(index, Entry.new(name, phone_number, email))
-   end
-   
-   def remove_entry(name, phone_number, email)
-   end
- end
+    end
+    
+    def remove_entry(name, phone_number, email)
+    end
+end
--- bloc/spec/address_book_spec.rb
+++ bloc/spec/address_book_spec.rb
@@ -26,4 +26,22 @@ require_relative '../models/address_book'
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
+   
+   describe "#remove_entry" do
+       it"removes only one entry to the address book" do
+           book = AddressBook.new
+           book.add_entry('John Stoccel', '999.898.7976', 'the.king@lovelace.com')
+ 
+           name = 'Ada Lovelace'
+           phone_number = '010.012.1815'
+           email = 'augusta.king@lovelace.com'
+           book.add_entry(name, phone_number, email)
+           
+           expect(book.entry.size).to eq(2)
+           book.remove_entry(name, phone_number, email)
+           expect(book.entry.size).to eq(1)
+       end
+       
+   end
+   
  end
